7f8b8d0186b1e4a65be7e3a225acceb9c2a87943
m4rcus06 <inormalz32@gmail.com>
2024-09-27 21:46:29 +0700

    New default template

--- templates/content/main_temp.cpp
+++ templates/content/main_temp.cpp
@@ -1,27 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-#define st first
-#define nd second
-
-typedef unsigned long long ull;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
+//replace with your directory
+#ifdef LOCAL
+#include <_>
+#else
+#define debug(...) 
+#endif
+ 
 void solve() {
-
 }
-
+ 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-
-	ll t = 1;
-	while (t--) {
-		solve();
-	}
-
-	return 0;
+    std::cin.tie(0)->sync_with_stdio(0);
+#ifdef LOCAL
+    auto begin = std::chrono::high_resolution_clock::now();
+#endif
+ 
+    int tt = 1;
+    while (tt--) {
+        solve();
+    }
+ 
+#ifdef LOCAL
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    std::cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+#endif
+ 
+    return 0;
 }
