cb912c692b416feb50a884d3697f33f2b7746807
m4rcus06 <inormalz32@gmail.com>
2025-08-17 19:05:07 +0700

    update

---
+++ main/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long;
+
+#ifdef LOCAL
+#include </home/marcus06/CP/Library/debug.h>
+#else
+#define debug(...) 
+#endif
+
+/*How to use:
+  Tvector <int, 2> g(n); //graph
+  Tvector <int, 3> f(n, k, 2) = f[n][k][2]
+*/
+template <class Tp, int D = 1>
+struct Tvector : public vector<Tvector<Tp, D - 1>> {
+  template <class... Args>
+  Tvector(int n = 0, Args... args) : vector<Tvector<Tp, D - 1>>(n, Tvector<Tp, D - 1>(args...)) {}
+};
+ 
+template <class Tp>
+struct Tvector<Tp, 1> : public vector<Tp> {
+  Tvector(int n = 0, Tp val = Tp()) : vector<Tp>(n, val) {}
+};
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    cout << "HEHEHEHEHE";
+}
+
+int main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+#ifdef LOCAL
+    auto begin = std::chrono::high_resolution_clock::now();
+#endif
+
+    int tt = 1;
+    while (tt--) {
+        solve();
+    }
+
+#ifdef LOCAL
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    std::cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+#endif
+    return 0;
+}
+
